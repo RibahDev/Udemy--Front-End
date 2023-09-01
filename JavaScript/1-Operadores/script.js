bebba500fd3e9b58cb07f328b5ae8cf485f84cf0
Ribeiro <joao.ribeiro17@outlook.com>
2023-08-31 23:59:11 -0300

    Introdução JS

---
+++ JavaScript/1-Operadores/script.js
@@ -0,0 +1,32 @@
+// Number
+console.log(typeof 2);
+console.log(typeof 5.14);
+console.log(typeof -2);
+
+// Operações Aritméticas
+console.log(2 + 4);
+console.log(10 - 5);
+console.log (5 * 4);
+console.log(10 / 2);
+
+console.log(5 + (8 * 2));
+
+// Special numbers
+console.log(typeof Infinity);
+console.log(typeof -Infinity);
+console.log (2 * "asd");
+console.log(typeof NaN);
+
+// Strings
+console.log("Hello World");
+console.log(`Olá Mundo`);
+console.log(typeof"Hello World");
+console.log(typeof `Olá Mundo`);
+
+// Simbolos especiais em string
+console.log("Testando a \n quebra de linha");
+console.log("Espaçamento \t de tab");
+
+// Concatenalção
+console.log("Oi," + "tudo" + " bem?")
+console.log(`Testando` + ` Com` + ` Crase`)
