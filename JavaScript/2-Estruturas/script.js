93e47265e43ada6bbbdc124c2ffbc7ff91f4638d
Ribeiro <joao.ribeiro17@outlook.com>
2023-09-01 10:46:27 -0300

    Aprofundamento de variáveis

---
+++ JavaScript/2-Estruturas/script.js
@@ -0,0 +1,28 @@
+// Variaveis
+let nome = "Joao";
+
+console.log(nome);
+console.log("Joao");
+
+nome = "João Ribeiro";
+
+console.log(nome);
+
+const idade = 31; 
+
+console.log(idade);
+// const cria uma variável de valor único
+//  erro ao alterar o valor "const idade = 32;"
+
+console.log(typeof idade);
+console.log(typeof nome);
+
+let a = 10;
+    b = 20;
+    c = 30;
+
+console.log(a, b, c);
+// variação 
+let nomesobrenome = "Vitor"
+let nomeSobrenome = "Ribeiro"
+console.log(`João ${nomesobrenome} ${nomeSobrenome}`)
