f79e5bcb13a2e402ce723bf773c3cdbd2497964b
RibahDev <joao.ribeiro17@outlook.com>
2023-09-06 15:38:00 -0300

    finalizando métodos

--- JavaScript/5-Métodos-string/scrpi.js
+++ JavaScript/5-Métodos-string/scrpi.js
@@ -8,3 +8,76 @@ console.log(trimTeste.trim());
 
 console.log(trimTeste.length);
 console.log(trimTeste.trim().length);
+
+// padStart
+const padStartTest = "1";
+
+const newNumber = padStartTest.padStart(4, "0");
+
+console.log(padStartTest);
+console.log(newNumber);
+
+const newNumber2 = newNumber.padEnd(10, "0");
+
+console.log(newNumber2);
+
+// split
+const frase = "O rato roeu a roupa do rei de Roma";
+
+const arrayDaFrase = frase.split(" ");
+console.log(arrayDaFrase);
+
+// join
+const fraseDenovo = arrayDaFrase.join(" ");
+
+console.log(fraseDenovo);
+
+const itensParaComprar = ["Mouse", "Teclado", "Monitor"]
+
+const fraseDeCompra = `Precisamos comprar: ${itensParaComprar.join(", ")}.`;
+
+console.log(fraseDeCompra);
+
+// Repeat
+const palavra = "Testando ";
+
+console.log(palavra.repeat(5));
+
+// Rest Operator
+const somaInfinita = (...args) => {
+    let total = 0;
+    for(i = 0; i < args.length; i++) {
+        total +=args[i];
+    }
+    return total;
+};
+console.log(somaInfinita(3, 3, 3));
+console.log(somaInfinita(17, 546, 5634, 31, 35890));
+
+// for...of
+const somaInfinita2 = (...args) => {
+    let total= 0
+        for (num of args) { 
+            total += num;
+        }
+        return total;
+}
+
+console.log(somaInfinita2(2, 2, 5, 7, 8));
+
+// Destructuring em objetos
+const userDetails = {
+    firstName: "João",
+    lastName: "Ribeiro",
+    job: "Programador",
+};
+
+const { firstName, lastName, job} = userDetails;
+console.log(firstName, lastName, job);
+
+// renomear variaveis
+const { firstName: primeiroNome} = userDetails;
+console.log(firstName)
+console.log(primeiroNome);
+
+// Destructuring em arrays
