b32aa68967f6e8deedd16044e9bc48671328d610
RibahDev <joao.ribeiro17@outlook.com>
2023-09-13 10:43:59 -0300

    Conclusão da seção JavaScript no navegador

--- JavaScript/9-DOM/js/script.js
+++ JavaScript/9-DOM/js/script.js
@@ -40,3 +40,45 @@ const h2 = document.createElement("h2");
 h2.textContent = "Meu novo Título!";
 
 header.replaceChild(h2, title);
+
+// createTextNode
+
+const myText = document.createTextNode("Agora vamos colocar mais um título");
+console.log(myText);
+
+const h3 = document.createElement("h3");
+
+h3.appendChild(myText);
+
+console.log(h3);
+
+mainContainer.appendChild(h3);
+
+// trabalhando com atributos
+const fisrtLink = navLinks.querySelector("a");
+console.log(fisrtLink);
+fisrtLink.setAttribute("href", "https://www.google.com");
+console.log(fisrtLink.getAttribute("href"));
+fisrtLink.setAttribute("target", "_blank");
+
+// trabalhando dimensões dos elementos
+const footer = document.querySelector("footer");
+console.log(footer.offsetWidth);
+console.log(footer.offsetHeight);
+
+console.log(footer.clientWidth)
+console.log(footer.clientHeight);
+
+// posição do elemento
+const product1 = product[0];
+console.log(product1.getBoundingClientRect());
+
+// Estilos com Js
+mainContainer.style.color = "red";
+mainContainer.style.backgroundColor = "#333";
+mainContainer.style.paddingBottom = "10px"
+
+// HTMLCollection
+for(const li of listItens) {
+    li.style.backgroundColor = "red";
+}
