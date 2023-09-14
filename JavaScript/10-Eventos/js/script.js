85c271b7174d535b93648e6e9f0607946d6f8f30
RibahDev <joao.ribeiro17@outlook.com>
2023-09-14 10:43:19 -0300

    Conclusão da sessão de Eventos em JS

--- JavaScript/10-Eventos/js/script.js
+++ JavaScript/10-Eventos/js/script.js
@@ -66,5 +66,71 @@ document.addEventListener("keydown", (e) => {
 const mouseEvents = document.querySelector("#mouse");
 
 mouseEvents.addEventListener("mousedown", () => {
-    console.log(mouseEvent);
-})
+    console.log("Pressionou o botão");
+});
+
+mouseEvents.addEventListener("mouseup", () => {
+    console.log("Soltou o botão");
+});
+
+mouseEvents.addEventListener("dblclick", () => {
+    console.log("Clique duplo");
+});
+
+/*// Movimentos do mouse
+document.addEventListener("mousemove", (e) => {
+    console.log(`No eixo X:${e.x}`);
+    console.log(`No eixo Y: ${e.y}`);
+});*/
+
+// Eventos de scroll
+window.addEventListener("scroll", (e) => {
+    if(window.pageYOffset > 200) {
+    console.log("Passamos de 200px");
+    }    
+});
+
+// Eventos de foco
+const input = document.querySelector("#my-input");
+
+input.addEventListener("focus", (e) => {
+    console.log("Entrou no input")
+});
+
+input.addEventListener("blur", (e) => {
+    console.log("Saiu do input")
+});
+
+// Eventos de carregamentos de Pagina
+window.addEventListener("load", () => {
+    console.log("A pagina carregou!");
+});
+
+window.addEventListener("beforeunload", (e) => {
+    e.preventDefault();
+    e.returnValue = "teste";
+});
+
+// Debounce
+const debounce = (f, delay) => {
+    let timeout;
+
+    return (...arguments) => {
+        if(timeout) {
+            clearTimeout(timeout)
+        }
+
+        timeout = setTimeout(() => {
+            f.apply(arguments)
+        }, delay);
+    };
+};
+
+window.addEventListener(
+    "mousemove",
+    debounce(() => {
+        console.log("Executando a cada 400ms");
+    },400)
+);
+
+
