30518d8cd6272a592e81863b7de9c3146270a0e8
RibahDev <joao.ribeiro17@outlook.com>
2023-09-23 16:21:35 -0300

    Iniciando em Axios

---
+++ JavaScript/Axios/js/script.js
@@ -0,0 +1,19 @@
+// Instalação
+console.log(axios);
+
+// Primeiro request
+const getData = async() => {
+
+    try {
+
+        const response = await axios.get("https://jsonplaceholder.typicode.com/users");
+
+        console.log(response);
+
+        return response.data;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+getData();
