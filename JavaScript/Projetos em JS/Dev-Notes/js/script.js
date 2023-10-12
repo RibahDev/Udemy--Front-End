a48dec897f5ea88343c030152cad12f2b03a74b8
RibahDev <joao.ribeiro17@outlook.com>
2023-10-12 11:25:41 -0300

    estilizando

--- JavaScript/Projetos em JS/Dev-Notes/js/script.js
+++ JavaScript/Projetos em JS/Dev-Notes/js/script.js
@@ -1,4 +1,6 @@
+
 // Elementos
+
 const notesContainer = document.querySelector("#notes-container");
 const noteInput = document.querySelector("#note-content");
 const addNoteBtn = document.querySelector(".add-note");
