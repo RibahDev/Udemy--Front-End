739cebb0912ce95eb754eb0d02aceb2699f9fe76
RibahDev <joao.ribeiro17@outlook.com>
2023-10-21 12:17:03 -0300

    Iniciando Css e JS do projeto

---
+++ JavaScript/Projetos em JS/Gerador-Box-Shadow/js/script.js
@@ -0,0 +1,35 @@
+class BoxShadowGenerator {
+
+    constructor(horizontal, horizontalRef, vertical, verticalRef, blur, blurRef, spread, spreadRef, rule, webkitRule, mozRule) {
+        
+    this.horizontal = horizontal;
+    this.horizontalRef = horizontalRef;
+    this.vertical = vertical;
+    this.verticalRef = verticalRef;
+    this.blur = blur;
+    this.blurRef = blurRef;
+    this.spread = spread;
+    this.spreadRef = spreadRef;
+    this.rule = rule;
+    this.webkitRule = webkitRule;
+    this.mozRule = mozRuleq;
+
+    }
+}
+
+// Seleção de Elementos
+const horizontal = document.querySelector("#horizontal");
+const horizontalRef = document.querySelector("#horizontal-value");
+const vertical = document.querySelector("#vertical");
+const verticalRef = document.querySelector("#vertical-value");
+const blur = document.querySelector("#blur");
+const blurRef = document.querySelector("#blur-value");
+const spread = document.querySelector("#spread");
+const spreadRef = document.querySelector("#spread-value");
+
+const previewBox = document.querySelector("#box");
+
+const rule = document.querySelector("#rule span");
+const webkitRule = document.querySelector("#webkit-rule span");
+const mozRule = document.querySelector("#moz-rule span");
+// Eventos
