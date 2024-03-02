6c89441fe4978d86479ac2f72c1a95e7525e7a64
Ribeiro <joao.ribeiro17@outlook.com>
2024-03-02 13:56:41 -0300

    Else

--- React/Avançando em React/avancando/src/components/ConditionalRender.jsx
+++ React/Avançando em React/avancando/src/components/ConditionalRender.jsx
@@ -1,14 +1,27 @@
 import React from 'react'
 
 const ConditionalRender = () => {
-    const x = 3;
-  
+  const x = 3;
+
+  const name = "João"
   return <div>
-     {/*7 - Render Condicional  */}
-     <h3>Isso será exibido?</h3>
-     {x > 2 && <p>Se x for True, Sim</p>}
-    </div>
-  
+    {/*7 - Render Condicional  */}
+    <h3>Isso será exibido?</h3>
+    {x > 2 && <p>Se x for True, Sim</p>}
+    {/* 8 - Else */}
+    <h3>Render Ternário</h3>
+    {name === "João" ? (
+      <div>
+        <p>Olá João!</p>
+      </div>
+    ) : (
+      <div>
+        <p>Nome não encontrado!</p>
+      </div>
+    )}
+
+  </div>
+
 };
 
 export default ConditionalRender
